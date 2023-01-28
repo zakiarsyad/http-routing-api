80771166184681b24a6754c14fbc050e7d21d1bd
zakiarsyad <zakiarsyad@gmail.com>
2023-01-28 21:15:42 +0700

    [routing-api] feat: forward request to destination server

--- routing-api/src/app.ts
+++ routing-api/src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import axios from "axios"
 
 const app = express();
 const port = process.env.PORT || 8080;
+const urls = process.env.URLS || "http://localhost:3001,http://localhost:3002,http://localhost:3003";
+
+const servers = urls.split(",")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +15,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
     '/',
     async (req, res) => {
-        res.status(200).send({});
+        try {
+            const response = await axios({
+                url: servers[0],
+                method: req.method,
+                data: req.body
+            });
+
+            res.status(response.status).send(response.data || {});
+        } catch (err) {
+            res.status(err.status || 500).send(err.data || {});
+        }
     }
 );
 
